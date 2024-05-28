35709c4f4d5106448e6b960b281cdb5945f0ba60
Jan Gluźniewicz <Jan.gluzniewicz@gmail.com>
2024-05-29 01:09:21 +0200

    test updated

--- src/test/java/intelligence/artificial/logic/MultiLayerPerceptronTest.java
+++ src/test/java/intelligence/artificial/logic/MultiLayerPerceptronTest.java
@@ -7,7 +7,6 @@ import intelligence.artificial.dao.DataLoaderInterface;
 import org.deeplearning4j.datasets.iterator.utilty.ListDataSetIterator;
 import org.deeplearning4j.nn.api.Layer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
-import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.junit.jupiter.api.Test;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
