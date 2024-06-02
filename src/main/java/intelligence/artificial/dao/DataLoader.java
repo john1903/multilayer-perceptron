e11d6e525b4395e9adc668601701ac6bc92536d8
Jan Gluźniewicz <Jan.gluzniewicz@gmail.com>
2024-06-02 20:18:18 +0200

    data loading improved

--- src/main/java/intelligence/artificial/dao/DataLoader.java
+++ src/main/java/intelligence/artificial/dao/DataLoader.java
@@ -6,6 +6,7 @@ import org.nd4j.linalg.factory.Nd4j;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DataLoader implements DataLoaderInterface {
@@ -13,19 +14,37 @@ public class DataLoader implements DataLoaderInterface {
     public INDArray[] loadData(String filePath) throws IOException {
 
         List<String> lines = Files.readAllLines(Paths.get(filePath));
-        double[][] inputData = new double[2][lines.size()];
-        double[][] outputData = new double[2][lines.size()];
+        List<double[]> inputDataList = new ArrayList<>();
+        List<double[]> outputDataList = new ArrayList<>();
 
-        for (int i = 0; i < lines.size(); i++) {
-            String[] values = lines.get(i).split(",");
-            inputData[0][i] = Double.parseDouble(values[0]);
-            inputData[1][i] = Double.parseDouble(values[1]);
-            outputData[0][i] = Double.parseDouble(values[2]);
-            outputData[1][i] = Double.parseDouble(values[3]);
+        for (String line : lines) {
+            String[] values = line.split(",");
+            try {
+                double input1 = Double.parseDouble(values[0]);
+                double input2 = Double.parseDouble(values[1]);
+                double output1 = Double.parseDouble(values[2]);
+                double output2 = Double.parseDouble(values[3]);
+
+                inputDataList.add(new double[]{input1, input2});
+                outputDataList.add(new double[]{output1, output2});
+            } catch (NumberFormatException e) {
+                System.out.println("Skipping line: " + line + " in file: " + filePath);
+            }
         }
 
-        INDArray inputs = Nd4j.createFromArray(inputData).transpose();
-        INDArray outputs = Nd4j.createFromArray(outputData).transpose();
+        double[][] inputData = new double[inputDataList.size()][2];
+        double[][] outputData = new double[outputDataList.size()][2];
+
+        for (int i = 0; i < inputDataList.size(); i++) {
+            inputData[i] = inputDataList.get(i);
+        }
+
+        for (int i = 0; i < outputDataList.size(); i++) {
+            outputData[i] = outputDataList.get(i);
+        }
+
+        INDArray inputs = Nd4j.createFromArray(inputData);
+        INDArray outputs = Nd4j.createFromArray(outputData);
 
         return new INDArray[]{inputs, outputs};
     }
